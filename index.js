98b7ff82013c214183c46fb8baeebb6f51445177
winphyoethu <phyoegyee@gmail.com>
2019-02-28 16:03:57 +0630

    [UPDATE]

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
-        console.log("LOGS :: ",event)
+        console.log("LOG :: ",event)
         if (event.message && event.message.text) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
