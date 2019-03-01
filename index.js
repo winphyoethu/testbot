9eb1c073fcf8720c34a50a010987d4947205a9fe
winphyoethu <phyoegyee@gmail.com>
2019-03-01 11:50:07 +0630

    [UPDATE]

--- index.js
+++ index.js
@@ -56,14 +56,14 @@ function setupGetStartedButton(res){
 function getDataFromRgo(res) {
     request({
         url: 'http://13.250.4.112/rgo47/public/api/web-api/product/AST8N003169_AST8N003119',
-        method: 'GET',
         headers: {
             'x-language' : 'en',
             "x-api-secret-key" : "7KG2D00LQrG1tKlTruzbujKCGVME0M3aOHN0yhsdEUNyLE6NVhS",
             "x-device-id" :"1234567",
             "x-app-version" : "1.1.1",
             "x-user-id" : "83596"
-        }
+        },
+        method: 'GET'
     },
     function(error, response, body) {
         if(!error) {
